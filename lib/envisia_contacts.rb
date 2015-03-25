3127a6548676814a0669458e49f7450f4980c472
Aldwin Ibuna <aldwin@apemangroup.com>
2015-03-26 05:36:20 +0800

    Initial commit
    Initial commit

---
+++ lib/envisia_contacts.rb
@@ -0,0 +1,5 @@
+require "envisia_contacts/version"
+
+module EnvisiaContacts
+  # Your code goes here...
+end
---
+++ test/minitest_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'envisia_contacts'
+
+require 'minitest/autorun'
